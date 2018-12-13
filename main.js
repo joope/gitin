33f0e2535abf4c190aab0efeb9255edd4955fdc7
joope <pyry.joona@helsinki.fi>
2018-12-14 00:26:50 +0200

    Update 3

--- main.js
+++ main.js
@@ -4,5 +4,5 @@ console.log('This script is repeating itself');
 console.log('This script is repeating itself');
 
 const p = document.createElement('p');
-p.textContent = 'This is an update 2';
+p.textContent = 'This is an update 3';
 document.body.appendChild(p);
