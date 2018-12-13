50e280fe2d0c0427e8d4730eca88a2ef6878e0f0
joope <pyry.joona@helsinki.fi>
2018-12-14 00:08:57 +0200

    Update script

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ console.log('This script is very useful');
 console.log('This script is very useful');
 console.log('This script is repeating itself');
 console.log('This script is repeating itself');
+
+const p = document.createElement('p');
+p.textContent = 'This is an update';
+document.body.appendChild(p);
