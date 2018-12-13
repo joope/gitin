4729113cea22649e9deed2b5fafa03992718da8c
joope <pyry.joona@helsinki.fi>
2018-12-13 23:11:12 +0200

    Change

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
 console.log('This script is very useful');
 console.log('This script is very useful');
 console.log('This script is repeating itself');
+console.log('This script is repeating itself');
